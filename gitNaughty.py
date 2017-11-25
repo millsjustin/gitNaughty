f33569086cbfb44f16b0ef34d394c9495ac5e6c5
Justin Mills <justin.mills@utah.edu>
2017-11-25 12:58:15 -0700

    refactored main function to check for abuse rate limit

--- gitNaughty.py
+++ gitNaughty.py
@@ -2,23 +2,49 @@ import requests
 import sys
 import time
 import datetime
-import jacksonsVerification as jackson
+import json
+import pickle
+import jacksonsVerification
+import justinsVerification
 
+# the state of the script will be saved in this file in json format
+# - the next api url
+# - the current min file size
+# - the current max file size
+state_filename = "api_state.txt"
+
+# load the github api token from a file
 with open("github_token.txt", "r") as token_file:
     github_token = token_file.read().strip()
-github_api_url = "https://api.github.com/search/code"
-search_pattern = jackson.get_search_pattern()
-GITHUB_API_MAX_FILESIZE = 383999 # < 384 KB
-INITIAL_MIN_FILESIZE = 20 # initialize (in bytes)
-INITIAL_MAX_FILESIZE = 40
-last_url_file = "last_url.txt"
+
+# file size paramaters
+GITHUB_API_MAX_FILESIZE = 383999  # < 384 KB
+INITIAL_MIN_FILESIZE = 1024  # initialize (in bytes)
+INITIAL_MAX_FILESIZE = 5120
+
+GITHUB_CODE_SEARCH_URL = "https://api.github.com/search/code"
+SEARCH_PATTERN = justinsVerification.private_key_search_pattern
 
 
-def verify(file_content: str, search_pattern: str):
-    jackson.verify(file_content, search_pattern)
+def verify(item: dict):
+    raw_url = get_raw_url(item)
+    raw_file_response = requests.get(raw_url)
+
+    if not raw_file_response.ok:
+        print("There was an error getting this raw file: {}".format(raw_url))
+        return
+
+    justinsVerification.verifyPrivateKey(raw_file_response.text)
 
 
-def check_rate_limit(count_remaining: str, reset_time: str):
+def check_rate_limit(headers: dict):
+    if "X-RateLimit-Remaining" not in headers or "X-RateLimit-Reset" not in headers:
+        print("Rate limit info was not in the api response headers")
+        return
+
+    count_remaining = headers["X-RateLimit-Remaining"]
+    reset_time = headers["X-RateLimit-Reset"]
+
     if count_remaining == "0":
         reset_time = datetime.datetime.fromtimestamp(float(reset_time))
         time_to_wait = reset_time - datetime.datetime.now()
@@ -26,6 +52,16 @@ def check_rate_limit(count_remaining: str, reset_time: str):
             return
         print("Rate Limit Hit, Sleeping: {}".format(time_to_wait.seconds + 1))
         time.sleep(time_to_wait.seconds + 1)
+
+
+def check_abuse_limit(api_response: requests.Response):
+    if "Retry-After" not in api_response.headers:
+        log_error_and_exit("api response returned status code: 403 and 'Retry-After' is not in the headers", api_response)
+
+    time_to_wait = int(api_response.headers["Retry-After"]) + 1
+    print("Abuse rate limit hit, Sleeping: {}".format(time_to_wait))
+    time.sleep(time_to_wait)
+
 
 
 def get_raw_url(item):
@@ -51,7 +87,7 @@ def update_filesize_window(min_filesize: int, max_filesize: int):
 
 
 def update_payload(min_filesize: int, max_filesize: int):
-    q = search_pattern + "+size:" + str(min_filesize) + ".." + str(max_filesize)
+    q = SEARCH_PATTERN + "+size:" + str(min_filesize) + ".." + str(max_filesize)
     payload = {"q": q, "access_token": github_token}
     return payload
 
@@ -67,39 +103,73 @@ def get_filesize_info(last_file_content: str):
     return [min_filesize, max_filesize, filesize_step]
 
 
+def log_error_and_exit(message: str, api_response: requests.Response):
+    print(message)
+    with open("api_response.pickle", "wb") as api_res_file:
+        pickle.dump(api_response, api_res_file)
+    cleanup_and_exit()
+
+
+def load_api_state():
+    with open(state_filename, "r") as state_file:
+        return json.load(state_file)
+
+
+def save_api_state(api_state: dict):
+    with open(state_filename, "w") as state_file:
+        json.dump(api_state, state_file)
+
+
 def main():
-    min_filesize = INITIAL_MIN_FILESIZE
-    max_filesize = INITIAL_MAX_FILESIZE
-    filesize_step = max_filesize - min_filesize
-    q = search_pattern + "+size:" + str(min_filesize) + ".." + str(max_filesize)
-    payload = {"q": q, "access_token": github_token}
+    api_state = {
+        "next_url": "",
+        "min_filesize": INITIAL_MIN_FILESIZE,
+        "max_filesize": INITIAL_MAX_FILESIZE
+    }
+    payload = {
+        "q": "{}+size:{}..{}".format(SEARCH_PATTERN, api_state["min_filesize"], api_state["max_filesize"]),
+        "access_token": github_token,
+        "per_page": 100
+    }
 
     if len(sys.argv) > 1:
-        with open(last_url_file, "r") as last_file:
-            last_file_content = last_file.read().strip()
-            [min_filesize, max_filesize, filesize_step] = get_filesize_info(last_file_content)
-            api_response = requests.get(last_file_content)
+        api_state = load_api_state()
+        api_response = requests.get(api_state["next_url"])
     else:
-        api_response = requests.get(github_api_url, params=payload)
+        api_response = requests.get(GITHUB_CODE_SEARCH_URL, params=payload)
 
+    while True:
+        if not api_response.ok:
+            if api_response.status_code == 403:
+                check_abuse_limit(api_response)
+                continue
+            else:
+                log_error_and_exit("There was an error with the github api: {}".format(api_response.text), api_response)
+
+        response_json = api_response.json()
+
+        if "items" not in response_json:
+            log_error_and_exit("The api response did not contain an 'items' field", api_response)
+
+        for item in response_json["items"]:
+            verify(item)
+
+        if "next" not in api_response.links:
+            log_error_and_exit("There was no 'next' item in the api headers", api_response)
+
+        api_state["next_url"] = api_response.links["next"]["url"]
+        save_api_state(api_state)
+
+        check_rate_limit(api_response.headers)
+
+        api_response = requests.get(api_state["next_url"])
+
+
+"""
     print("This many matches found: " + str(api_response.json()["total_count"]))
     while True:
 
         while "next" in api_response.links:
-            res_json = api_response.json()
-            items = res_json["items"]
-
-            for item in items:
-                raw_url = get_raw_url(item)
-                file_res = requests.get(raw_url)
-                verify(file_res.text, search_pattern)
-
-            with open(last_url_file, "w") as last_file:
-                last_file.write(api_response.links["next"]["url"])
-
-            check_rate_limit(api_response.headers["X-RateLimit-Remaining"], api_response.headers["X-RateLimit-Reset"])
-
-            api_response = requests.get(api_response.links["next"]["url"])
 
             # handle the last case
 
@@ -121,7 +191,14 @@ def main():
         else:
             print("Search has completed for all filesizes")
             break
+"""
+
+
+def cleanup_and_exit():
+    justinsVerification.stat_shelve.close()
+    sys.exit()
 
 
 if __name__ == "__main__":
     main()
+    cleanup_and_exit()
---
+++ justinsVerification.py
@@ -0,0 +1,23 @@
+import shelve
+import re
+
+private_key_search_pattern = "-----BEGIN RSA PRIVATE KEY-----"
+rsa_key_re = re.compile("-+BEGIN RSA PRIVATE KEY-+(.*?)-+END RSA PRIVATE KEY-+", flags=re.DOTALL)
+stat_shelve = shelve.open("privateKey.shelve")
+
+
+def verifyPrivateKey(file_content: str):
+    stat_key = ""
+    re_match = rsa_key_re.search(file_content)
+
+    if not re_match:
+        stat_key = file_content
+    else:
+        stat_key = re_match.group(1)
+
+    if stat_key not in stat_shelve:
+        stat_shelve[stat_key] = 1
+    else:
+        stat_shelve[stat_key] += 1
+
+    print(stat_key)
