67eaf96b092d47aa9ad254e91c9b469c2ff71f17
Jackson Murphy <jackson@Jacksons-MacBook-Pro.local>
2017-11-24 17:56:53 -0700

    added regex pattern for identifying actual tokens

--- jacksonsVerification.py
+++ jacksonsVerification.py
@@ -1,16 +1,39 @@
+import re
+
 total_results_count = [0]
 naughty_count = [0]
+tokens = set()
+
+def check_for_token(potential_token):
+    pattern = re.compile("[a-zA-Z0-9\.]{10,80}")
+    match_object = pattern.search(potential_token)
+    if match_object is None:
+        return [False, None]
+    else:
+        return [True, match_object.group(0)]
 
 def verify(file_content: str, search_pattern: str):
     total_results_count[0] += 1
-    print("\nResult #" + str(total_results_count[0]))
+    print("Processing results #" + str(total_results_count[0]))
+    start_index = -1 # initialize
     try:
         start_index = file_content.index(search_pattern)
-        if len(file_content[start_index:]) < 100:
-            end_index = start_index + len(file_content[start_index:])
-        else:
-            end_index = start_index + 100
-
-        print(file_content[start_index:end_index])
     except ValueError:
         print("ERROR: search pattern not found in file content")
+
+    if len(file_content[start_index:]) < 100:
+        end_index = start_index + len(file_content[start_index:])
+    else:
+        end_index = start_index + 100
+
+    potential_token = file_content[start_index:end_index]
+    [is_token, naughty_token] = check_for_token(potential_token)
+    if is_token:
+        naughty_count[0] += 1
+        tokens.add(naughty_token)
+
+    if total_results_count[0] == 800:
+        print("Found this many naughty tokens: " + str(naughty_count[0]))
+        with open("jacksonsResults.txt", "w") as results_file:
+            for token in tokens:
+                results_file.write(token + "\n")
